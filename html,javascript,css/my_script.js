e934b74720771af5a8959c1ca8960327b68b58e1
jousohlo <jouko.sohlo@gmail.com>
2014-08-06 20:25:59 +0300

    Playing around with JQuery

--- html,javascript,css/my_script.js
+++ html,javascript,css/my_script.js
@@ -1,10 +1,13 @@
 function myFunction(nbr1, txt) {
+    text_data = document.getElementById("data").value
     result = nbr1++;
-    document.getElementById("result").innerHTML = txt + result;
+    type = jQuery.isFunction(result);
+    $("#result").text(txt + result + type + text_data);
     var i;
     for (i = 0; i < 10; i++) {
         var newPara = document.createElement("p");
         newPara.appendChild(document.createTextNode("hello"+ i));
-        document.body.insertBefore(newPara, document.getElementById("result"));
+        document.body.insertBefore(newPara, document.getElementById("result")); 
     }
+    document.getElementById("title").innerHTML = "Playing with HTML, css and Javascript";
 }

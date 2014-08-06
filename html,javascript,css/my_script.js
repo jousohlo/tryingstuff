c37d332fca7bc58ea8666fb761882d27f3a990ff
jousohlo <jouko.sohlo@gmail.com>
2014-08-06 22:51:01 +0300

    Got connected to LinkedIn API

--- html,javascript,css/my_script.js
+++ html,javascript,css/my_script.js
@@ -1,8 +1,14 @@
 function myFunction(nbr1, txt) {
-    text_data = document.getElementById("data").value
-    result = nbr1++;
-    type = jQuery.isFunction(result);
-    $("#result").text(txt + result + type + text_data);
+    //get text from text field
+    //text_data = document.getElementById("data").value
+    //basic numeric calculation
+    //result = nbr1++;
+    
+    //insert some text with jQuery
+    //$("#result").text(txt + result + text_data);
+    //create html object with Javascript
+    
+    /*
     var i;
     for (i = 0; i < 10; i++) {
         var newPara = document.createElement("p");
@@ -10,4 +16,9 @@ function myFunction(nbr1, txt) {
         document.body.insertBefore(newPara, document.getElementById("result")); 
     }
     document.getElementById("title").innerHTML = "Playing with HTML, css and Javascript";
+    */
+    //Get firstname from my LinkedIn profile
+    IN.API.Profile("me").fields(["firstName"]).result(function(result) {
+        $("#result").text(result.values[0].firstName)})  
+   
 }
